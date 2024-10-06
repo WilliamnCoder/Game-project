533dec1e1ffa0e31451d76f7db24a879ad01c378
William <121023616+Userwr20@users.noreply.github.com>
2024-10-06 13:45:40 +0200

    conditional tr operator variant of Odd-even game done

--- GameProjectUsingTRoperators.js
+++ GameProjectUsingTRoperators.js
@@ -1,3 +1,6 @@
 // The game of Odd or Even using conditional (ternary) operators instead of if-else operator
 const number = Math.floor(Math.random() * 100) +1;
 
+const result = (number % 2 == 0) ? "even" : "odd";
+
+console.log (`The number ${number} is ${result}`);
