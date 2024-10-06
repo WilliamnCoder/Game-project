c65faa460ef00520ef42f4e28a88f205194c5649
William <121023616+Userwr20@users.noreply.github.com>
2024-10-06 13:53:51 +0200

    changed to "==" in if statement

--- GameProject.js
+++ GameProject.js
@@ -1,7 +1,7 @@
 // Using if-else operator for Odd or Even number
 const number = Math.floor(Math.random()*100)+1;  //generating a number between 1-100
 
-if (number % 2 === 0) {
+if (number % 2 == 0) {
     console.log (`${number} is an even number!`)
 } else {
     console.log (`${number} is an odd number!`)
